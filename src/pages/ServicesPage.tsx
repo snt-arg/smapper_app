20a56eb8734d6d4d9ce67350acfa91ce65bc35ac
Pedro Soares <pmbs.123@gmail.com>
2025-04-01 19:19:57 +0200

    feat: use services API client for service page

---
+++ src/pages/ServicesPage.tsx
@@ -0,0 +1,43 @@
+import { fetchServices } from "../api/services/ServiceAPI";
+import { useEffect, useState } from "react";
+import { Service } from "../types/Service";
+
+const ServicesPage = () => {
+  const [services, setServices] = useState<Service[]>([]); // Store the services
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    // Fetch services when the component mounts
+    const getServices = async () => {
+      try {
+        const services = await fetchServices();
+        setServices(services);
+      } catch (error) {
+        setError("Could not fetch services");
+      }
+    };
+
+    getServices();
+  }, []); // Empty dependency array means this runs only once when the component mounts
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Services</h1>
+      <ul>
+        {services.map((service) => (
+          <li key={service.id}>
+            <h2>{service.name}</h2>
+            <p>Status: {service.status}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ServicesPage;
+
