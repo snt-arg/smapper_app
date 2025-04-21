83a320b168884736b25a5591efe2be4db8539c3e
Pedro Soares <pmbs.123@gmail.com>
2025-04-21 17:25:39 +0200

    feat: intial impl for reading foxglove ws-protocol

--- src/pages/VisualizerPage.tsx
+++ src/pages/VisualizerPage.tsx
@@ -1,4 +1,7 @@
+import useFoxglove from '@/features/viewer/hooks/useFoxglove'
+
 function VisualizerPage() {
+  useFoxglove('ws://localhost:8765')
   return (
     <>
       <h1>This is the VisualizerPage</h1>
