2de39825dbfc7efa819a1119b50a160fab28135b
Pedro Soares <pmbs.123@gmail.com>
2025-04-24 20:36:33 +0200

    chore: remove foxglove

--- src/pages/VisualizerPage.tsx
+++ src/pages/VisualizerPage.tsx
@@ -1,7 +1,4 @@
-import useFoxglove from '@/features/viewer/hooks/useFoxglove'
-
 function VisualizerPage() {
-  useFoxglove('ws://localhost:8765')
   return (
     <>
       <h1>This is the VisualizerPage</h1>
