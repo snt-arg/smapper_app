97115bc0f43fe8cc2a1ad2ffbee82f8a13dba60c
Pedro Soares <pmbs.123@gmail.com>
2025-04-26 18:18:39 +0200

    feat: define API URL type

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL as string,
   timeout: 10000,
 })
 
