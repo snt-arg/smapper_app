b0301253ea49dfafc06cb04e6b2ce6c32c1ae2fb
Pedro Soares <pmbs.123@gmail.com>
2025-06-04 13:32:09 +0200

    feat: add a sandbox for rosboard as the visualizer page

--- eslint.config.js
+++ eslint.config.js
@@ -36,6 +36,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'react-dom/no-unsafe-iframe-sandbox': 'off',
     },
   }
 )
--- src/pages/VisualizerPage.tsx
+++ src/pages/VisualizerPage.tsx
@@ -1,8 +1,15 @@
 function VisualizerPage() {
   return (
-    <>
-      <h1>This is the VisualizerPage</h1>
-    </>
+    <div style={{ width: '100%', height: '100vh', border: 'none' }}>
+      <iframe
+        src="http://localhost:8888"
+        title="Embedded Website"
+        width="100%"
+        height="100%"
+        style={{ border: 'none' }}
+        sandbox="allow-scripts allow-same-origin"
+      />
+    </div>
   )
 }
 
